7818cea913ae0bb097c807a2db84870f5af5e42f
Subhrajit Das <133939116+iamsubhrajit10@users.noreply.github.com>
2023-05-19 17:56:02 +0530

    Update UpdatedPMCLAP.m

--- UpdatedPMCLAP.m
+++ UpdatedPMCLAP.m
@@ -5,212 +5,10 @@ global counters;
 counters=zeros(1,4);
 global cumProbabilites;
 cumProbabilites=zeros(1,4);
+instance_number=1;
 for i=1:10
     tic;
-    fit=PMCLAP_ABC(30,10,250,96,0.85,0);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,10,250,96,0.95,0);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,10,250,96,0.85,1);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,10,250,96,0.95,1);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,10,250,96,0.85,2);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,20,250,96,0.85,0);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,20,250,96,0.95,0);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-
-sum=0;
-fitM=[];
-totalTime=0;
-global counters;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,20,250,96,0.85,1);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,20,250,96,0.95,1);
+    fit=PMCLAP_ABC(30,10,250,96,0.85,0,i,instance_number);
     totalTime=totalTime+toc;
     sum=sum+fit;
     fitM(end+1)=fit;
@@ -230,59 +28,7 @@ counters
 
 
 
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,20,250,96,0.85,2);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-
-sum=0;
-fitM=[];
-totalTime=0;
-counters=zeros(1,4);
-cumProbabilites=zeros(1,4);
-for i=1:10
-    tic;
-    fit=PMCLAP_ABC(30,20,250,96,0.95,2);
-    totalTime=totalTime+toc;
-    sum=sum+fit;
-    fitM(end+1)=fit;
-end
-avg=sum/10;
-fprintf("\nAverage Fitness: %f ",avg);
-fprintf("+\nMax Fitness: %f ",max(fitM));
-y=0;
-for i=1:10
-    x=(fitM(1,i)-avg)^2;
-    y=y+x;
-end
-standardDeviation=(y/10)^0.5;
-fprintf("\nStandard Deviation: %f ",standardDeviation);
-fprintf("\nAverage Time: %f ",totalTime/10);
-counters
-
-
-function[fitmax]=PMCLAP_ABC(P,K,r,mu,alpha,b)
+function[fitmax]=PMCLAP_ABC(P,K,r,mu,alpha,b,increment,instance)
     x=mu*((1-alpha)^(1/(b+2)));
     data=readmatrix("C:\MCLP_GA\324.txt");         %Data Set Coordinate file path
     %file can be downloaded from: http://www.lac.inpe.br/~lorena/instances/mcover/Coord/coord818.txt
@@ -302,17 +48,46 @@ function[fitmax]=PMCLAP_ABC(P,K,r,mu,alpha,b)
         [eB,counter]=employeedBees(currentPop,P,K,distance,demand,r,nrows,x,epochs,counter);
         [oB,counter]=onlookerBees(eB,P,K,distance,demand,r,nrows,x,epochs,counter);
         [SP,counter]=scoutBees(oB,P,K,distance,demand,r,nrows,x,epochs,counter);
-%         population(:,:,epochs)=SP;
         [modifiedPop,~]=createNextGenerationFrom(SP,currentPop,P,K,r,demand,distance,nrows,x,epochs);
         eN=enhanceSolutionVector(modifiedPop, P, K, distance, demand, r, nrows, x,epochs);
-        [ population(:,:,epochs),fitness]=createNextGenerationFrom(eN,modifiedPop,P,K,r,demand,distance,nrows,x,epochs);
+        [ population(:,:,epochs),fitness,allocationPopulation]=createNextGenerationFrom(eN,modifiedPop,P,K,r,demand,distance,nrows,x,epochs);
         fitM(epochs,:,:)=fitness;     
     end
     fitmax=max(fitM(:));
     fprintf("\nRadius: %d, Facilities opened: %d",r,K);
     fprintf("\nEpochs: %d",epochs);
     fprintf("\nFitness: %f ",fitmax);
-    'Facility Indices',population(1,:,epochs)
+    
+    baseDirectory = 'C:\MCLP_GA\324R\';
+    instanceName = num2str(instance);
+    indiceFileName = 'indice_data';
+    subDirectory = fullfile(baseDirectory, instanceName);
+    if ~isfolder(subDirectory)
+        mkdir(subDirectory);
+    end
+    
+    filename = sprintf('%s_%d.txt', indiceFileName, increment);
+    fullFilePath = fullfile(subDirectory, filename);
+    indices=population(1,:,epochs);
+    fileID = fopen(fullFilePath, 'w');
+    % Write the matrix data to the file
+    fprintf(fileID, '%d\t', indices');
+    fprintf(fileID, '\n');
+    % Close the file
+    fclose(fileID);
+    
+    allocationFileName = 'allocation_data';
+    % Extract the first page from the reshaped matrix
+    firstMatrix = reshape(allocationPopulation(1, :, :), 1, nrows);
+
+    filename = sprintf('%s_%d.txt',  allocationFileName, increment);
+    fullFilePath = fullfile(subDirectory, filename);
+    fileID = fopen(fullFilePath, 'w');
+    % Write the matrix data to the file
+    fprintf(fileID, '%d\t', firstMatrix');
+    fprintf(fileID, '\n');
+    % Close the file
+    fclose(fileID);
 end
 
 
@@ -412,7 +187,7 @@ function [newPopulation,counter] = onlookerBees(population, P, K, distance, dema
 end
 
 function [population,counter] = scoutBees(population,P, K, distance, demand, r, nrows, x,epochs,counter)
-    L = 20;
+    L = floor(0.1*K*P);
     for i=1:P
         if counter(1,i)> L
             population(i, :) = randperm(nrows,K);
@@ -423,31 +198,31 @@ function [population,counter] = scoutBees(population,P, K, distance, demand, r, 
 end
 
 
-function [pool,fitness] = createNextGenerationFrom(population, T, P, K, r, demand, distance, nrows, x,epochs)
+function [pool,fitness,allocationPopulation] = createNextGenerationFrom(population, T, P, K, r, demand, distance, nrows, x,epochs)
     mergedPop = cat(1, population, T);
-    fitness = computePopulationFitness(mergedPop, 2 * P, K, r, demand, distance, nrows, x,epochs);
+    [fitness,allocationPopulation] = computePopulationFitness(mergedPop, 2 * P, K, r, demand, distance, nrows, x,epochs);
     [fitness, indices] = maxk(fitness, P);
     pool = mergedPop(indices,:);
-
 end
 
 
-function [fit]=computePopulationFitness(population,P,K,r,demand,distance,nrows,x,epochs)
+function [fit,allocationPopulation]=computePopulationFitness(population,P,K,r,demand,distance,nrows,x,epochs)
     fit=zeros(P,1);
+    allocationPopulation=zeros(P,1,nrows);
     for i=1:P
-        fit(i,1)=getFitness(population(i,:),K,r,demand,distance,nrows,x,epochs);
+        [fit(i,1),allocationMatrix]=getFitness(population(i,:),K,r,demand,distance,nrows,x,epochs);
+        allocationPopulation(i,1,:)=allocationMatrix;
     end
 end
-function[fitness]=getFitness(solution,K,r,demand,distance,nrows,x,epochs)
+function[fitness,allocationMatrix]=getFitness(solution,K,r,demand,distance,nrows,x,epochs)
    global counters;
    global cumProbabilites;
-   
+   tempAllocation=zeros(1,2,nrows);
    if epochs<50
-        fit1=getFitness1(solution,K,r,demand,distance,nrows,x,epochs);
-        fit2=getFitness2(solution,K,r,demand,distance,nrows,x);
-        fit3=getFitness3(solution,K,r,demand,distance,nrows,x);
-        fit4=getFitness4(solution,K,r,demand,distance,nrows,x);
-        [fitness,i]=max([fit1 fit2 fit3 fit4]);
+        [fit1,tempAllocation(1,1,:)]=getFitness1(solution,K,r,demand,distance,nrows,x,epochs);
+        [fit2,tempAllocation(1,2,:)]=getFitness2(solution,K,r,demand,distance,nrows,x);
+        [fitness,i]=max([fit1 fit2]);
+        allocationMatrix=tempAllocation(1,i,:);
         counters(1,i)=counters(1,i)+1;
    else
                % initialize eps to a small positive constant
@@ -457,9 +232,7 @@ function[fitness]=getFitness(solution,K,r,demand,distance,nrows,x,epochs)
             % compute probabilities using roulette wheel selection
             total = sum(counters(1,:));
             probabilites(1) = (counters(1,1) + eps) / (total + 4*eps);
-            probabilites(2) = (counters(1,2) + eps) / (total + 4*eps);
-            probabilites(3) = (counters(1,3) + eps) / (total + 4*eps);
-            probabilites(4) = (counters(1,4) + eps) / (total + 4*eps); 
+            probabilites(2) = (counters(1,2) + eps) / (total + 4*eps); 
 
             % compute cumulative probabilities
             cumProbabilites = cumsum(probabilites);
@@ -469,24 +242,20 @@ function[fitness]=getFitness(solution,K,r,demand,distance,nrows,x,epochs)
         randProb = rand();
         if randProb < cumProbabilites(1)
             index=1;
-            fitness = getFitness1(solution, K, r, demand, distance, nrows, x,epochs);
+            [fitness,allocationMatrix] = getFitness1(solution, K, r, demand, distance, nrows, x,epochs);
+            
         elseif randProb < cumProbabilites(2)
             index=2;
-            fitness = getFitness2(solution, K, r, demand, distance, nrows, x);
-        elseif randProb < cumProbabilites(3)
-            index=3;
-            fitness = getFitness3(solution, K, r, demand, distance, nrows, x);
-        elseif randProb < cumProbabilites(4)
-            index=4;
-            fitness = getFitness4(solution, K, r, demand, distance, nrows, x);
+            [fitness,allocationMatrix] = getFitness2(solution, K, r, demand, distance, nrows, x);
         else
             index=1;
-            fitness = getFitness2(solution, K, r, demand, distance, nrows, x);
+            [fitness,allocationMatrix] = getFitness1(solution, K, r, demand, distance, nrows, x);
         end
        counters(1,index)=counters(1,index)+1;
    end
 end
-function[fitness]=getFitness1(solution,K,r,demand,distance,nrows,x,epochs)
+
+function[fitness,allocation]=getFitness1(solution,K,r,demand,distance,nrows,x,epochs)
     val=0;
     yM=zeros(K,1);
     allocation=zeros(1,nrows);
@@ -501,7 +270,7 @@ function[fitness]=getFitness1(solution,K,r,demand,distance,nrows,x,epochs)
     end
     fitness=val;
 end
-function[fitness]=getFitness2(solution,K,r,demand,distance,nrows,x)
+function[fitness,allocation]=getFitness2(solution,K,r,demand,distance,nrows,x)
     val=0;
     yM=zeros(K,1);
     allocation=zeros(1,nrows);
@@ -512,48 +281,6 @@ function[fitness]=getFitness2(solution,K,r,demand,distance,nrows,x)
                 val=val+demand(i);
                 allocation(1,i)=solution(facilityNo);
             end
-        end
-    end
-    fitness=val;
-end
-function[fitness]=getFitness3(solution,K,r,demand,distance,nrows,x)
-    val=0;
-    yM=zeros(K,1);
-    allocation=zeros(1,nrows);
-    for i=1:nrows
-        if allocation(1,i) ==0
-            [yM,facilityNo,flag] = getNearestFacility(solution,i,distance,r,yM,x,demand,K);
-             if flag
-                val=val+demand(i);
-                allocation(1,i)=solution(facilityNo);
-            end
-        end
-    end
-    fitness=val;
-end
-
-function[fitness]=getFitness4(solution,K,r,demand,distance,nrows,x)
-    val=0;
-    yM=zeros(K,1);
-    allocation=zeros(1,nrows);
-    weightedMatrix=zeros(1,nrows);
-    myArray = 1:K;
-    permIndices = randperm(numel(myArray));
-    uniqueValues = unique(myArray(permIndices));
-    for i=1:numel(uniqueValues)
-        currentValue = uniqueValues(i);
-        for j=1:nrows
-            f=0.01*demand(j);
-            if ~allocation(1,j) && (yM(currentValue,1)+f)<=x && distance(solution(currentValue),j)<=r
-                weightedMatrix(1,j)=demand(j)/distance(solution(currentValue),j);
-            end
-        end
-        [maxW,index]=max(weightedMatrix(1,:));
-        if maxW ~=0
-            allocation(1,index)=currentValue;
-            val=val+demand(index);
-            f=0.01*demand(index);
-            yM(currentValue,1)=yM(currentValue,1)+f;
         end
     end
     fitness=val;
@@ -605,25 +332,6 @@ function[yM,facilityNo,flag]=getLessCongestedFacility(solution,customer,distance
       flag=true;
   end
 end
-function[yM,facilityNo,flag]=getNearestFacility(solution,customer,distance,r,yM,x,demand,K)
-  availableFacility=zeros(1,K);
-  flag=false;
-  facilityNo=-1;
-  f=0.01*demand(customer);
-  min=distance(solution(1),customer);
-  for i=1:K
-      if distance(solution(i),customer)<=r && (yM(i,1)+f)<=x && distance(solution(i),customer)<min
-          min=distance(solution(i),customer);
-          facilityNo=i;
-          availableFacility(1,i)=1;
-      end
-  end
-  if facilityNo ~=-1
-      yM(facilityNo,1)=yM(facilityNo,1)+f;
-      flag=true;
-  end
-end
-
 
 function [neighbour] = getNeighbours(facility, N, distance, nrows)
     d = Inf(nrows, 1);
